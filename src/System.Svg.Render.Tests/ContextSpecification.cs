5f6f240b9de6bda5dbf5194a1f0237e3e9ebcd90
Andreas Niedermair <me+code@a76a6a65.nexus>
2024-11-27 23:53:58 +0100

    perf: added additional layer for future implementations

---
+++ src/System.Svg.Render.Tests/ContextSpecification.cs
@@ -0,0 +1,32 @@
+﻿// see http://mrclyfar.blogspot.co.at/2010/02/amazing-mapping-demo-at-ted-2010.html
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+// ReSharper disable CheckNamespace
+
+namespace UnitTest
+{
+  public abstract class ContextSpecification
+  {
+    public TestContext TestContext { get; set; }
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+      this.Context();
+      this.BecauseOf();
+    }
+
+    [TestCleanup]
+    public void TestCleanup()
+    {
+      this.Cleanup();
+    }
+
+    protected virtual void Context() {}
+
+    protected virtual void BecauseOf() {}
+
+    protected virtual void Cleanup() {}
+  }
+}
