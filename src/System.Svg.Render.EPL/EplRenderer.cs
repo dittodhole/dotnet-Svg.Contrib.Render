7ccb2b481930453c0bad659cb341b27dc0fc7ca5
Andreas Niedermair <me+code@a76a6a65.nexus>
2024-11-27 23:50:44 +0100

    refactored encoding in EplRenderer
    clients need it, but the instance itself not ...

--- src/System.Svg.Render.EPL/EplRenderer.cs
+++ src/System.Svg.Render.EPL/EplRenderer.cs
@@ -18,7 +18,6 @@ namespace System.Svg.Render.EPL
       this.EplCommands = eplCommands;
       this.PrinterCodepage = printerCodepage;
       this.CountryCode = countryCode;
-      this.Encoding = this.CreateEncoding(this.PrinterCodepage);
     }
 
     [NotNull]
@@ -29,15 +28,58 @@ namespace System.Svg.Render.EPL
 
     protected PrinterCodepage PrinterCodepage { get; }
 
-    [NotNull]
-    private Encoding Encoding { get; }
-
     protected int CountryCode { get; }
 
     [NotNull]
     public Encoding GetEncoding()
     {
-      return this.Encoding;
+      switch (this.PrinterCodepage)
+      {
+        case PrinterCodepage.Dos347:
+          return Encoding.GetEncoding(347);
+        case PrinterCodepage.Dos850:
+          return Encoding.GetEncoding(850);
+        case PrinterCodepage.Dos852:
+          return Encoding.GetEncoding(852);
+        case PrinterCodepage.Dos860:
+          return Encoding.GetEncoding(860);
+        case PrinterCodepage.Dos863:
+          return Encoding.GetEncoding(863);
+        case PrinterCodepage.Dos865:
+          return Encoding.GetEncoding(865);
+        case PrinterCodepage.Dos857:
+          return Encoding.GetEncoding(857);
+        case PrinterCodepage.Dos861:
+          return Encoding.GetEncoding(861);
+        case PrinterCodepage.Dos862:
+          return Encoding.GetEncoding(862);
+        case PrinterCodepage.Dos855:
+          return Encoding.GetEncoding(855);
+        case PrinterCodepage.Dos866:
+          return Encoding.GetEncoding(866);
+        case PrinterCodepage.Dos737:
+          return Encoding.GetEncoding(737);
+        case PrinterCodepage.Dos851:
+          return Encoding.GetEncoding(851);
+        case PrinterCodepage.Dos869:
+          return Encoding.GetEncoding(869);
+        case PrinterCodepage.Windows1252:
+          return Encoding.GetEncoding(1252);
+        case PrinterCodepage.Windows1250:
+          return Encoding.GetEncoding(1250);
+        case PrinterCodepage.Windows1251:
+          return Encoding.GetEncoding(1251);
+        case PrinterCodepage.Windows1253:
+          return Encoding.GetEncoding(1253);
+        case PrinterCodepage.Windows1254:
+          return Encoding.GetEncoding(1254);
+        case PrinterCodepage.Windows1255:
+          return Encoding.GetEncoding(1255);
+        default:
+          // TODO !
+          // :beers: should never happen
+          throw new NotImplementedException();
+      }
     }
 
     [NotNull]
@@ -144,60 +186,6 @@ namespace System.Svg.Render.EPL
       eplStream.Add(this.EplCommands.Print(1));
 
       return eplStream;
-    }
-
-    [NotNull]
-    private Encoding CreateEncoding(PrinterCodepage printerCodepage)
-    {
-      switch (printerCodepage)
-      {
-        case PrinterCodepage.Dos347:
-          return Encoding.GetEncoding(347);
-        case PrinterCodepage.Dos850:
-          return Encoding.GetEncoding(850);
-        case PrinterCodepage.Dos852:
-          return Encoding.GetEncoding(852);
-        case PrinterCodepage.Dos860:
-          return Encoding.GetEncoding(860);
-        case PrinterCodepage.Dos863:
-          return Encoding.GetEncoding(863);
-        case PrinterCodepage.Dos865:
-          return Encoding.GetEncoding(865);
-        case PrinterCodepage.Dos857:
-          return Encoding.GetEncoding(857);
-        case PrinterCodepage.Dos861:
-          return Encoding.GetEncoding(861);
-        case PrinterCodepage.Dos862:
-          return Encoding.GetEncoding(862);
-        case PrinterCodepage.Dos855:
-          return Encoding.GetEncoding(855);
-        case PrinterCodepage.Dos866:
-          return Encoding.GetEncoding(866);
-        case PrinterCodepage.Dos737:
-          return Encoding.GetEncoding(737);
-        case PrinterCodepage.Dos851:
-          return Encoding.GetEncoding(851);
-        case PrinterCodepage.Dos869:
-          return Encoding.GetEncoding(869);
-        case PrinterCodepage.Windows1252:
-          return Encoding.GetEncoding(1252);
-        case PrinterCodepage.Windows1250:
-          return Encoding.GetEncoding(1250);
-        case PrinterCodepage.Windows1251:
-          return Encoding.GetEncoding(1251);
-        case PrinterCodepage.Windows1253:
-          return Encoding.GetEncoding(1253);
-        case PrinterCodepage.Windows1254:
-          return Encoding.GetEncoding(1254);
-        case PrinterCodepage.Windows1255:
-          return Encoding.GetEncoding(1255);
-        default:
-          // TODO !
-          // :beers: should never happen
-          throw new ArgumentOutOfRangeException(nameof(printerCodepage),
-                                                printerCodepage,
-                                                null);
-      }
     }
   }
 }
