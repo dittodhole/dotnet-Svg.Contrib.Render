a2db05bdecd1bc6fb686725f1334fc7ff81de570
Andreas Niedermair <me+code@a76a6a65.nexus>
2024-11-27 23:54:09 +0100

    adapted transformation of SvgRectangle for FingerPrint

--- src/Svg.Contrib.Render.FingerPrint/FingerPrintTransformer.cs
+++ src/Svg.Contrib.Render.FingerPrint/FingerPrintTransformer.cs
@@ -1,4 +1,5 @@
-﻿using JetBrains.Annotations;
+﻿using System.Drawing.Drawing2D;
+using JetBrains.Annotations;
 
 namespace Svg.Contrib.Render.FingerPrint
 {
@@ -19,5 +20,28 @@ namespace Svg.Contrib.Render.FingerPrint
       : base(svgUnitReader,
              outputWidth,
              outputHeight) {}
+
+    [Pure]
+    public override void Transform([NotNull] SvgRectangle svgRectangle,
+                                   [NotNull] Matrix matrix,
+                                   out float startX,
+                                   out float startY,
+                                   out float endX,
+                                   out float endY,
+                                   out float strokeWidth)
+    {
+      base.Transform(svgRectangle,
+                     matrix,
+                     out startX,
+                     out startY,
+                     out endX,
+                     out endY,
+                     out strokeWidth);
+
+      startX -= strokeWidth / 2f;
+      endX += strokeWidth / 2f;
+      startY -= strokeWidth / 2f;
+      endY += strokeWidth / 2f;
+    }
   }
 }
