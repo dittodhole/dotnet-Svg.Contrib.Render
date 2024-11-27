80325fd2edcdbd680e9ed56a58cd56171c16b71c
Andreas Niedermair <me+code@a76a6a65.nexus>
2024-11-27 23:50:47 +0100

    added System.Svg.Render.ZPL.Demo

---
+++ src/System.Svg.Render.ZPL.Demo/CustomBootstrapper.cs
@@ -0,0 +1,30 @@
+﻿using JetBrains.Annotations;
+
+namespace System.Svg.Render.ZPL.Demo
+{
+  [PublicAPI]
+  public class CustomBootstrapper : DefaultBootstrapper
+  {
+    [NotNull]
+    [Pure]
+    [MustUseReturnValue]
+    protected override System.Svg.Render.ZPL.ZplTransformer CreateZplTransformer([NotNull] SvgUnitReader svgUnitReader)
+    {
+      return new ZplTransformer(svgUnitReader);
+    }
+
+    //[NotNull]
+    //[Pure]
+    //[MustUseReturnValue]
+    //protected override System.Svg.Render.ZPL.SvgImageTranslator CreateSvgImageTranslator([NotNull] System.Svg.Render.ZPL.ZplTransformer zplTransformer,
+    //                                                                                     [NotNull] ZplCommands zplCommands,
+    //                                                                                     bool assumeStoredInInternalMemory)
+    //{
+    //  return new SvgImageTranslator(zplTransformer,
+    //                                zplCommands)
+    //         {
+    //           AssumeStoredInInternalMemory = assumeStoredInInternalMemory
+    //         };
+    //}
+  }
+}
