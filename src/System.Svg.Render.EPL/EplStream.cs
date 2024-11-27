705bc2e25ac214cbc406f6054d39a86a8b08cfb3
Andreas Niedermair <me+code@a76a6a65.nexus>
2024-11-27 23:50:43 +0100

    added EplStream
    this will be the new representation of streams to the printer ...

---
+++ src/System.Svg.Render.EPL/EplStream.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JetBrains.Annotations;
+
+namespace System.Svg.Render.EPL
+{
+  public class EplStream
+  {
+    protected ICollection<object> InternalStream { get; } = new LinkedList<object>();
+
+    public void Add([NotNull] string s)
+    {
+      this.InternalStream.Add(s);
+    }
+
+    public void Add([NotNull] byte[] array)
+    {
+      this.InternalStream.Add(array);
+    }
+
+    public void Add([NotNull] EplStream eplStream)
+    {
+      foreach (var line in eplStream.InternalStream)
+      {
+        this.InternalStream.Add(line);
+      }
+    }
+
+    public IEnumerable<byte> ToByteArray([NotNull] Encoding encoding)
+    {
+      foreach (var line in this.InternalStream)
+      {
+        byte[] array;
+        var s = line as string;
+        if (s != null)
+        {
+          array = encoding.GetBytes(s);
+        }
+        else
+        {
+          array = line as byte[];
+        }
+
+        if (array == null)
+        {
+          continue;
+        }
+        if (!array.Any())
+        {
+          continue;
+        }
+
+        foreach (var @byte in array)
+        {
+          yield return @byte;
+        }
+
+        foreach (var @byte in encoding.GetBytes(Environment.NewLine))
+        {
+          yield return @byte;
+        }
+      }
+    }
+  }
+}
