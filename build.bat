32355b146b46324b0a9195a2b6235b16c1a14e8d
Andreas Niedermair <me+code@a76a6a65.nexus>
2024-11-27 23:54:11 +0100

    go go, appveyor go :dragon:

--- build.bat
+++
@@ -1,3 +0,0 @@
-powershell-nuget-packager\NuGet.exe restore src/Svg.Contrib.Render.sln
-"%PROGRAMFILES(x86)%\MSBuild\14.0\Bin\MsBuild.exe" src/Svg.Contrib.Render.sln
-powershell powershell-nuget-packager\Package.ps1
