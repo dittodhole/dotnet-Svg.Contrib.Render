7352e7bd5dcda234b60e0f2ac0ecc1a0f4f5f72b
Andreas Niedermair <me+code@a76a6a65.nexus>
2024-11-27 23:53:55 +0100

    added SvgUnitCalculator.GetDevicePoints

--- src/System.Svg.Render.EPL.Tests/SvgUnitCalculatorSpecs.cs
+++ src/System.Svg.Render.EPL.Tests/SvgUnitCalculatorSpecs.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using UnitTest;
+
 // ReSharper disable InconsistentNaming
 
 namespace System.Svg.Render.EPL.Tests
@@ -97,6 +98,399 @@ namespace System.Svg.Render.EPL.Tests
       {
         Assert.AreEqual(this.Actual,
                         200);
+      }
+    }
+
+    [TestClass]
+    public class when_inch_is_converted_to_device_points : SvgUnitCalculatorSpecsContext
+    {
+      protected override void BecauseOf()
+      {
+        base.BecauseOf();
+
+        var svgUnit = new SvgUnit(SvgUnitType.Inch,
+                                  3f);
+        this.Actual = this.SvgUnitCalculator.GetDevicePoints(svgUnit,
+                                                             203);
+      }
+
+      [TestMethod]
+      public void result_should_be_correct()
+      {
+        Assert.AreEqual(609,
+                        this.Actual);
+      }
+    }
+
+    [TestClass]
+    public class when_untyped_value_is_mapped_to_inches_and_converted_to_device_points : SvgUnitCalculatorSpecsContext
+    {
+      protected override void Context()
+      {
+        base.Context();
+
+        this.SvgUnitCalculator.NoneSubstution = SvgUnitType.Inch;
+      }
+
+      protected override void BecauseOf()
+      {
+        base.BecauseOf();
+
+        var svgUnit = new SvgUnit(SvgUnitType.None,
+                                  3f);
+        this.Actual = this.SvgUnitCalculator.GetDevicePoints(svgUnit,
+                                                             203);
+      }
+
+      [TestMethod]
+      public void result_should_be_correct()
+      {
+        Assert.AreEqual(609,
+                        this.Actual);
+      }
+    }
+
+
+    [TestClass]
+    public class when_centimeter_is_converted_to_device_points : SvgUnitCalculatorSpecsContext
+    {
+      protected override void BecauseOf()
+      {
+        base.BecauseOf();
+
+        var svgUnit = new SvgUnit(SvgUnitType.Centimeter,
+                                  10f);
+        this.Actual = this.SvgUnitCalculator.GetDevicePoints(svgUnit,
+                                                             203);
+      }
+
+      [TestMethod]
+      public void result_should_be_correct()
+      {
+        Assert.AreEqual(799,
+                        this.Actual);
+      }
+    }
+
+    [TestClass]
+    public class when_untyped_value_is_mapped_to_centimeters_and_converted_to_device_points : SvgUnitCalculatorSpecsContext
+    {
+      protected override void Context()
+      {
+        base.Context();
+
+        this.SvgUnitCalculator.NoneSubstution = SvgUnitType.Centimeter;
+      }
+
+      protected override void BecauseOf()
+      {
+        base.BecauseOf();
+
+        var svgUnit = new SvgUnit(SvgUnitType.Centimeter,
+                                  10f);
+        this.Actual = this.SvgUnitCalculator.GetDevicePoints(svgUnit,
+                                                             203);
+      }
+
+      [TestMethod]
+      public void result_should_be_correct()
+      {
+        Assert.AreEqual(799,
+                        this.Actual);
+      }
+    }
+
+
+    [TestClass]
+    public class when_millimeter_is_converted_to_device_points : SvgUnitCalculatorSpecsContext
+    {
+      protected override void BecauseOf()
+      {
+        base.BecauseOf();
+
+        var svgUnit = new SvgUnit(SvgUnitType.Millimeter,
+                                  8433f);
+        this.Actual = this.SvgUnitCalculator.GetDevicePoints(svgUnit,
+                                                             203);
+      }
+
+      [TestMethod]
+      public void result_should_be_correct()
+      {
+        Assert.AreEqual(67397,
+                        this.Actual);
+      }
+    }
+
+    [TestClass]
+    public class when_untyped_value_is_mapped_to_millimeters_and_converted_to_device_points : SvgUnitCalculatorSpecsContext
+    {
+      protected override void Context()
+      {
+        base.Context();
+
+        this.SvgUnitCalculator.NoneSubstution = SvgUnitType.Millimeter;
+      }
+
+      protected override void BecauseOf()
+      {
+        base.BecauseOf();
+
+        var svgUnit = new SvgUnit(SvgUnitType.None,
+                                  8433f);
+        this.Actual = this.SvgUnitCalculator.GetDevicePoints(svgUnit,
+                                                             203);
+      }
+
+      [TestMethod]
+      public void result_should_be_correct()
+      {
+        Assert.AreEqual(67397,
+                        this.Actual);
+      }
+    }
+
+    [TestClass]
+    public class when_pixel_is_converted_to_device_points : SvgUnitCalculatorSpecsContext
+    {
+      protected override void BecauseOf()
+      {
+        base.BecauseOf();
+
+        var svgUnit = new SvgUnit(SvgUnitType.Pixel,
+                                  100f);
+        this.Actual = this.SvgUnitCalculator.GetDevicePoints(svgUnit,
+                                                             203);
+      }
+
+      [TestMethod]
+      public void result_should_be_correct()
+      {
+        Assert.AreEqual(281,
+                        this.Actual);
+      }
+    }
+
+    [TestClass]
+    public class when_untyped_value_is_mapped_to_pixels_and_converted_to_device_points : SvgUnitCalculatorSpecsContext
+    {
+      protected override void Context()
+      {
+        base.Context();
+
+        this.SvgUnitCalculator.NoneSubstution = SvgUnitType.Pixel;
+      }
+
+      protected override void BecauseOf()
+      {
+        base.BecauseOf();
+
+        var svgUnit = new SvgUnit(SvgUnitType.None,
+                                  100f);
+        this.Actual = this.SvgUnitCalculator.GetDevicePoints(svgUnit,
+                                                             203);
+      }
+
+      [TestMethod]
+      public void result_should_be_correct()
+      {
+        Assert.AreEqual(281,
+                        this.Actual);
+      }
+    }
+
+
+    [TestClass]
+    public class when_pica_is_converted_to_device_points : SvgUnitCalculatorSpecsContext
+    {
+      protected override void BecauseOf()
+      {
+        base.BecauseOf();
+
+        var svgUnit = new SvgUnit(SvgUnitType.Pica,
+                                  128840f);
+        this.Actual = this.SvgUnitCalculator.GetDevicePoints(svgUnit,
+                                                             203);
+      }
+
+      [TestMethod]
+      public void result_should_be_correct()
+      {
+        Assert.AreEqual(36325,
+                        this.Actual);
+      }
+    }
+
+    [TestClass]
+    public class when_untyped_value_is_mapped_to_pica_and_converted_to_device_points_and_translated : SvgUnitCalculatorSpecsContext
+    {
+      protected override void Context()
+      {
+        base.Context();
+
+        this.SvgUnitCalculator.NoneSubstution = SvgUnitType.Pica;
+      }
+
+      protected override void BecauseOf()
+      {
+        base.BecauseOf();
+
+        var svgUnit = new SvgUnit(SvgUnitType.None,
+                                  128840f);
+        this.Actual = this.SvgUnitCalculator.GetDevicePoints(svgUnit,
+                                                             203);
+      }
+
+      [TestMethod]
+      public void result_should_be_correct()
+      {
+        Assert.AreEqual(36325,
+                        this.Actual);
+      }
+    }
+
+    [TestClass]
+    public class when_em_is_converted_to_device_points : SvgUnitCalculatorFailingSpecsContext
+    {
+      protected override void BecauseOf()
+      {
+        base.BecauseOf();
+
+        var svgUnit = new SvgUnit(SvgUnitType.Em,
+                                  100f);
+
+        try
+        {
+          this.SvgUnitCalculator.GetDevicePoints(svgUnit,
+                                                 203);
+        }
+        catch (NotImplementedException notImplementedException)
+        {
+          this.Actual = notImplementedException;
+        }
+      }
+
+      [TestMethod]
+      public void should_get_not_implemented_exception()
+      {
+        Assert.IsInstanceOfType(this.Actual,
+                                typeof(NotImplementedException));
+      }
+    }
+
+    [TestClass]
+    public class when_ex_is_converted_to_device_points : SvgUnitCalculatorFailingSpecsContext
+    {
+      protected override void BecauseOf()
+      {
+        base.BecauseOf();
+
+        var svgUnit = new SvgUnit(SvgUnitType.Ex,
+                                  100f);
+
+        try
+        {
+          this.SvgUnitCalculator.GetDevicePoints(svgUnit,
+                                                 203);
+        }
+        catch (NotImplementedException notImplementedException)
+        {
+          this.Actual = notImplementedException;
+        }
+      }
+
+      [TestMethod]
+      public void should_get_not_implemented_exception()
+      {
+        Assert.IsInstanceOfType(this.Actual,
+                                typeof(NotImplementedException));
+      }
+    }
+
+    [TestClass]
+    public class when_percentage_is_converted_to_device_points : SvgUnitCalculatorFailingSpecsContext
+    {
+      protected override void BecauseOf()
+      {
+        base.BecauseOf();
+
+        var svgUnit = new SvgUnit(SvgUnitType.Percentage,
+                                  100f);
+
+        try
+        {
+          this.SvgUnitCalculator.GetDevicePoints(svgUnit,
+                                                 203);
+        }
+        catch (NotImplementedException notImplementedException)
+        {
+          this.Actual = notImplementedException;
+        }
+      }
+
+      [TestMethod]
+      public void should_get_not_implemented_exception()
+      {
+        Assert.IsInstanceOfType(this.Actual,
+                                typeof(NotImplementedException));
+      }
+    }
+
+    [TestClass]
+    public class when_user_value_is_converted_to_device_points : SvgUnitCalculatorFailingSpecsContext
+    {
+      protected override void BecauseOf()
+      {
+        base.BecauseOf();
+
+        var svgUnit = new SvgUnit(SvgUnitType.User,
+                                  100f);
+
+        try
+        {
+          this.SvgUnitCalculator.GetDevicePoints(svgUnit,
+                                                 203);
+        }
+        catch (NotImplementedException notImplementedException)
+        {
+          this.Actual = notImplementedException;
+        }
+      }
+
+      [TestMethod]
+      public void should_get_not_implemented_exception()
+      {
+        Assert.IsInstanceOfType(this.Actual,
+                                typeof(NotImplementedException));
+      }
+    }
+
+    [TestClass]
+    public class when_untyped_value_is_converted_to_device_points : SvgUnitCalculatorFailingSpecsContext
+    {
+      protected override void BecauseOf()
+      {
+        base.BecauseOf();
+
+        var svgUnit = new SvgUnit(SvgUnitType.None,
+                                  100f);
+
+        try
+        {
+          this.SvgUnitCalculator.GetDevicePoints(svgUnit,
+                                                 203);
+        }
+        catch (NotImplementedException notImplementedException)
+        {
+          this.Actual = notImplementedException;
+        }
+      }
+
+      [TestMethod]
+      public void should_get_not_implemented_exception()
+      {
+        Assert.IsInstanceOfType(this.Actual,
+                                typeof(NotImplementedException));
       }
     }
   }
