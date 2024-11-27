0447d367979073c65e0c9b1b8f6b09484a42a32c
Andreas Niedermair <me+code@a76a6a65.nexus>
2024-11-27 23:53:55 +0100

    added "LW" command and fixed tests

--- src/System.Svg.Render.EPL/SvgLineTranslator.cs
+++ src/System.Svg.Render.EPL/SvgLineTranslator.cs
@@ -1,4 +1,6 @@
-﻿namespace System.Svg.Render.EPL
+﻿using System.Drawing;
+
+namespace System.Svg.Render.EPL
 {
   public class SvgLineTranslator : SvgElementTranslator<SvgLine>
   {
@@ -22,55 +24,78 @@
 
       var startX = this.SvgUnitCalculator.GetValue(instance.StartX);
       var startY = this.SvgUnitCalculator.GetValue(instance.StartY);
-      var endX =this.SvgUnitCalculator.GetValue(instance.EndX);
+      var endX = this.SvgUnitCalculator.GetValue(instance.EndX);
       var endY = this.SvgUnitCalculator.GetValue(instance.EndY);
       var strokeWidth = this.SvgUnitCalculator.GetValue(instance.StrokeWidth);
 
       string translation;
       if (startY == endY
-          && startX < endX)
+          || startX == endX)
       {
-        // horizontal
-        var horizontalStart = startX;
-        var verticalStart = startY;
-        var horizontalLength = endX - startX;
-        if (horizontalLength == 0)
-        {
-          horizontalLength = strokeWidth;
-        }
-        var verticalLength = strokeWidth;
+        // horizontal or vertical
+        string command;
 
-        translation = $"LO{horizontalStart},{verticalStart},{horizontalLength},{verticalLength}";
-      }
-      else if (startX == endX
-               && startY < endY)
-      {
-        // vertical
-        var horizontalStart = startX;
-        var verticalStart = startY;
-        var horizontalLength = strokeWidth;
-        var verticalLength = endY - startY;
-        if (verticalLength == 0)
+        var stroke = instance.Stroke as SvgColourServer;
+        if (stroke?.Colour == Color.White)
         {
-          verticalLength = strokeWidth;
+          command = "LW";
+        }
+        else
+        {
+          command = "LO";
         }
 
-        translation = $"LO{horizontalStart},{verticalStart},{horizontalLength},{verticalLength}";
+        if (startX < endX)
+        {
+          // horizontal
+          var horizontalStart = startX;
+          var verticalStart = startY;
+          var horizontalLength = endX - startX;
+          if (horizontalLength == 0)
+          {
+            horizontalLength = strokeWidth;
+          }
+          var verticalLength = strokeWidth;
+
+          translation = $"{command}{horizontalStart},{verticalStart},{horizontalLength},{verticalLength}";
+        }
+        else if (startY < endY)
+        {
+          // vertical
+          var horizontalStart = startX;
+          var verticalStart = startY;
+          var horizontalLength = strokeWidth;
+          var verticalLength = endY - startY;
+          if (verticalLength == 0)
+          {
+            verticalLength = strokeWidth;
+          }
+
+          translation = $"{command}{horizontalStart},{verticalStart},{horizontalLength},{verticalLength}";
+        }
+        else
+        {
+          // TODO add documentation
+          throw new NotImplementedException();
+        }
       }
-      else if (startX < endX
-               && startY < endY)
+      else if (startX <= endX
+               && startY <= endY)
       {
         // diagonal
+        var command = "LS";
+
         var horizontalStart = startX;
         var verticalStart = startY;
         var horizontalLength = strokeWidth;
         var verticalLength = endX;
         var verticalEnd = endY;
 
-        translation = $"LS{horizontalStart},{verticalStart},{horizontalLength},{verticalLength},{verticalEnd}";
+        translation = $"{command}{horizontalStart},{verticalStart},{horizontalLength},{verticalLength},{verticalEnd}";
       }
       else
       {
+        // TODO add documentation
         throw new NotImplementedException();
       }
 
