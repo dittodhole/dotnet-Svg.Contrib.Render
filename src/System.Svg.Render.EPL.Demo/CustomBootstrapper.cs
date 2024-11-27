55d465bdc76d2e1faf8c779241a0f90ecdf5634f
Andreas Niedermair <me+code@a76a6a65.nexus>
2024-11-27 23:50:45 +0100

    restructured project
    added System.Svg.Render.EPL.Demo

---
+++ src/System.Svg.Render.EPL.Demo/CustomBootstrapper.cs
@@ -0,0 +1,30 @@
+﻿using JetBrains.Annotations;
+
+namespace System.Svg.Render.EPL.Demo
+{
+  [PublicAPI]
+  public class CustomBootstrapper : DefaultBootstrapper
+  {
+    [NotNull]
+    [Pure]
+    [MustUseReturnValue]
+    protected override System.Svg.Render.EPL.EplTransformer CreateEplTransformer([NotNull] SvgUnitReader svgUnitReader)
+    {
+      return new EplTransformer(svgUnitReader);
+    }
+
+    [NotNull]
+    [Pure]
+    [MustUseReturnValue]
+    protected override System.Svg.Render.EPL.SvgImageTranslator CreateSvgImageTranslator([NotNull] System.Svg.Render.EPL.EplTransformer eplTransformer,
+                                                                                         [NotNull] EplCommands eplCommands,
+                                                                                         bool assumeStoredInInternalMemory)
+    {
+      return new SvgImageTranslator(eplTransformer,
+                                    eplCommands)
+             {
+               AssumeStoredInInternalMemory = assumeStoredInInternalMemory
+             };
+    }
+  }
+}
---
+++ src/System.Svg.Render.EPL.Demo/EplTransformer.cs
@@ -0,0 +1,121 @@
+﻿using System.Drawing.Drawing2D;
+using JetBrains.Annotations;
+
+// ReSharper disable NonLocalizedString
+
+namespace System.Svg.Render.EPL.Demo
+{
+  [PublicAPI]
+  public class EplTransformer : System.Svg.Render.EPL.EplTransformer
+  {
+    public EplTransformer([NotNull] SvgUnitReader svgUnitReader)
+      : base(svgUnitReader) {}
+
+    public override void GetFontSelection([NotNull] SvgTextBase svgTextBase,
+                                          float fontSize,
+                                          out int fontSelection,
+                                          out int horizontalMultiplier,
+                                          out int verticalMultiplier)
+    {
+      if (svgTextBase.ID == "tspan5668")
+      {
+        fontSelection = 1;
+        horizontalMultiplier = 1;
+        verticalMultiplier = 1;
+      }
+      else if (svgTextBase.ID == "tspan5670")
+      {
+        fontSelection = 1;
+        horizontalMultiplier = 1;
+        verticalMultiplier = 1;
+      }
+      else if (svgTextBase.ID == "tspan5676")
+      {
+        fontSelection = 1;
+        horizontalMultiplier = 1;
+        verticalMultiplier = 1;
+      }
+      else if (svgTextBase.ID == "tspan5682")
+      {
+        fontSelection = 1;
+        horizontalMultiplier = 1;
+        verticalMultiplier = 1;
+      }
+      else if (svgTextBase.ID == "tspan3131")
+      {
+        fontSelection = 2;
+        horizontalMultiplier = 1;
+        verticalMultiplier = 1;
+      }
+      else if (svgTextBase.ID == "tspan5686-2")
+      {
+        fontSelection = 3;
+        horizontalMultiplier = 1;
+        verticalMultiplier = 1;
+      }
+      else if (svgTextBase.ID == "tspan5686-2-3")
+      {
+        fontSelection = 3;
+        horizontalMultiplier = 1;
+        verticalMultiplier = 1;
+      }
+      else
+      {
+        base.GetFontSelection(svgTextBase,
+                              fontSize,
+                              out fontSelection,
+                              out horizontalMultiplier,
+                              out verticalMultiplier);
+      }
+    }
+
+    public override void Transform(SvgTextBase svgTextBase,
+                                   Matrix matrix,
+                                   out float startX,
+                                   out float startY,
+                                   out float fontSize)
+    {
+      base.Transform(svgTextBase,
+                     matrix,
+                     out startX,
+                     out startY,
+                     out fontSize);
+
+      if (svgTextBase.ID == "tspan5668")
+      {
+        startX -= 100f;
+      }
+      else if (svgTextBase.ID == "tspan5670")
+      {
+        startX -= 100f;
+      }
+      else if (svgTextBase.ID == "tspan5676")
+      {
+        startX -= 100f;
+      }
+      else if (svgTextBase.ID == "tspan5682")
+      {
+        startX -= 100f;
+      }
+      else if (svgTextBase.ID == "tspan3131")
+      {
+        startX -= 10f;
+        startY += 15f;
+      }
+      else if (svgTextBase.ID == "tspan5686-2")
+      {
+        startX += 30f;
+        startY += 10f;
+      }
+      else if (svgTextBase.ID == "tspan5686-2-3")
+      {
+        startX += 30f;
+        startY += 10f;
+      }
+      else if (svgTextBase.ID == "tspan4665")
+      {
+        startY -= 30f;
+      }
+    }
+  }
+}
