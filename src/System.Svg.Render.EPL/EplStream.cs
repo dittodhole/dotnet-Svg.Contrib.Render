02588f025e35db5d5828bd6628b10a47cfff7a60
Andreas Niedermair <me+code@a76a6a65.nexus>
2024-11-27 23:54:02 +0100

    extracted MixedStream from EplStream

--- src/System.Svg.Render.EPL/EplStream.cs
+++ src/System.Svg.Render.EPL/EplStream.cs
@@ -5,43 +5,20 @@ using JetBrains.Annotations;
 
 namespace System.Svg.Render.EPL
 {
-  public class EplStream
+  public class EplStream : MixedStream
   {
-    protected ICollection<object> InternalStream { get; } = new LinkedList<object>();
-
-    public virtual bool IsEmpty => !this.InternalStream.Any();
-
-    public virtual void Add([NotNull] string s)
-    {
-      this.InternalStream.Add(s);
-    }
-
-    public virtual void Add([NotNull] IEnumerable<byte> buffer)
-    {
-      var array = buffer.ToArray();
-
-      this.Add(array);
-    }
-
-    public virtual void Add([NotNull] byte[] array)
-    {
-      this.InternalStream.Add(array);
-    }
-
     public virtual void Add([NotNull] EplStream eplStream)
     {
-      foreach (var line in eplStream.InternalStream)
+      foreach (var line in eplStream)
       {
-        this.InternalStream.Add(line);
+        this.AddElement(line);
       }
     }
 
-    public virtual byte[] ToByteArray([NotNull] Encoding encoding) => this.ToByteStream(encoding)
-                                                                          .ToArray();
-
-    public virtual IEnumerable<byte> ToByteStream([NotNull] Encoding encoding)
+    [NotNull]
+    public override IEnumerable<byte> ToByteStream([NotNull] Encoding encoding)
     {
-      foreach (var line in this.InternalStream)
+      foreach (var line in this)
       {
         byte[] array;
         var s = line as string;
@@ -73,14 +50,6 @@ namespace System.Svg.Render.EPL
           yield return @byte;
         }
       }
-    }
-
-    public override string ToString()
-    {
-      var result = string.Join(Environment.NewLine,
-                               this.InternalStream.OfType<string>());
-
-      return result;
     }
   }
 }
