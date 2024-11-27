f43f645d17cc086c154caa7fe3cd1bc57aba4093
Andreas Niedermair <me+code@a76a6a65.nexus>
2024-11-27 23:50:40 +0100

    added todo in SvgDocumentTranslator

--- src/System.Svg.Render.EPL/SvgDocumentTranslator.cs
+++ src/System.Svg.Render.EPL/SvgDocumentTranslator.cs
@@ -59,6 +59,7 @@ namespace System.Svg.Render.EPL
 
       if (svgElement is SvgTextSpan)
       {
+        // TODO remove this in a later version, this should be internal of SvgTextTranslator
         return;
       }
 
