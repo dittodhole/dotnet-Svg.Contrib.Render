46aaf3c9766a1a7257129a40fc460a69874715d8
Andreas Niedermair <me+code@a76a6a65.nexus>
2024-11-27 23:50:44 +0100

    minimizing complexity :beers:

--- src/System.Svg.Render.EPL/EplRenderer.cs
+++ src/System.Svg.Render.EPL/EplRenderer.cs
@@ -33,53 +33,10 @@ namespace System.Svg.Render.EPL
     [NotNull]
     public Encoding GetEncoding()
     {
-      switch (this.PrinterCodepage)
-      {
-        case PrinterCodepage.Dos347:
-          return Encoding.GetEncoding(347);
-        case PrinterCodepage.Dos850:
-          return Encoding.GetEncoding(850);
-        case PrinterCodepage.Dos852:
-          return Encoding.GetEncoding(852);
-        case PrinterCodepage.Dos860:
-          return Encoding.GetEncoding(860);
-        case PrinterCodepage.Dos863:
-          return Encoding.GetEncoding(863);
-        case PrinterCodepage.Dos865:
-          return Encoding.GetEncoding(865);
-        case PrinterCodepage.Dos857:
-          return Encoding.GetEncoding(857);
-        case PrinterCodepage.Dos861:
-          return Encoding.GetEncoding(861);
-        case PrinterCodepage.Dos862:
-          return Encoding.GetEncoding(862);
-        case PrinterCodepage.Dos855:
-          return Encoding.GetEncoding(855);
-        case PrinterCodepage.Dos866:
-          return Encoding.GetEncoding(866);
-        case PrinterCodepage.Dos737:
-          return Encoding.GetEncoding(737);
-        case PrinterCodepage.Dos851:
-          return Encoding.GetEncoding(851);
-        case PrinterCodepage.Dos869:
-          return Encoding.GetEncoding(869);
-        case PrinterCodepage.Windows1252:
-          return Encoding.GetEncoding(1252);
-        case PrinterCodepage.Windows1250:
-          return Encoding.GetEncoding(1250);
-        case PrinterCodepage.Windows1251:
-          return Encoding.GetEncoding(1251);
-        case PrinterCodepage.Windows1253:
-          return Encoding.GetEncoding(1253);
-        case PrinterCodepage.Windows1254:
-          return Encoding.GetEncoding(1254);
-        case PrinterCodepage.Windows1255:
-          return Encoding.GetEncoding(1255);
-        default:
-          // TODO !
-          // :beers: should never happen
-          throw new NotImplementedException();
-      }
+      var codepage = (int) this.PrinterCodepage;
+      var encoding = Encoding.GetEncoding(codepage);
+
+      return encoding;
     }
 
     [NotNull]
