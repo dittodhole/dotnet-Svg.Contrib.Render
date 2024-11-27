39a676f77383037efc134972f4cbb3e15718325a
Andreas Niedermair <me+code@a76a6a65.nexus>
2024-11-27 23:50:50 +0100

    added svgElementTranslatorBase for FingerPrint

---
+++ src/Svg.Contrib.Render.FingerPrint/SvgElementTranslatorBase.cs
@@ -0,0 +1,8 @@
+﻿using JetBrains.Annotations;
+
+namespace Svg.Contrib.Render.FingerPrint
+{
+  [PublicAPI]
+  public abstract class SvgElementTranslatorBase<TSvgElement> : SvgElementTranslatorBase<FingerPrintContainer, TSvgElement>
+    where TSvgElement : SvgElement {}
+}
