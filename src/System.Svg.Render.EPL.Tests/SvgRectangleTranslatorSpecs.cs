7680c4fb813f3469ecc103d7591d2dd0d0b9ed59
Andreas Niedermair <me+code@a76a6a65.nexus>
2024-11-27 23:50:38 +0100

    added translation of SvgRectangle

---
+++ src/System.Svg.Render.EPL.Tests/SvgRectangleTranslatorSpecs.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTest;
+
+namespace System.Svg.Render.EPL.Tests
+{
+  public static class SvgRectangleTranslatorSpecs
+  {
+    public class SvgRectangleTranslatorSpecsContext : ContextSpecification
+    {
+      public SvgRectangleTranslatorSpecsContext()
+      {
+        var svgUnitCalculator = new SvgUnitCalculator();
+
+        this.SvgLineTranslator = new SvgLineTranslator(svgUnitCalculator);
+        this.SvgRectangleTranslator = new SvgRectangleTranslator(this.SvgLineTranslator,
+                                                                 svgUnitCalculator);
+      }
+
+      protected SvgRectangleTranslator SvgRectangleTranslator { get; set; }
+      protected SvgLineTranslator SvgLineTranslator { get; set; }
+      protected SvgRectangle SvgRectangle { get; set; }
+      protected SvgLine UpperLine { get; set; }
+      protected SvgLine RightLine { get; set; }
+      protected SvgLine LowerLine { get; set; }
+      protected SvgLine LeftLine { get; set; }
+      protected object ActualUpperLine { get; set; }
+      protected object ActualRightLine { get; set; }
+      protected object ActualLowerLine { get; set; }
+      protected object ActualLeftLine { get; set; }
+    }
+
+    [TestClass]
+    public class when_valid_specs_are_given : SvgRectangleTranslatorSpecsContext
+    {
+      protected override void Context()
+      {
+        base.Context();
+
+        this.SvgRectangle = new SvgRectangle
+                            {
+                              X = new SvgUnit(10f),
+                              Y = new SvgUnit(10f),
+                              Width = new SvgUnit(100f),
+                              Height = new SvgUnit(100f),
+                              StrokeWidth = new SvgUnit(20f)
+                            };
+      }
+
+      protected override void BecauseOf()
+      {
+        base.BecauseOf();
+
+        this.UpperLine = this.SvgRectangleTranslator.GetUpperLine(this.SvgRectangle);
+        this.RightLine = this.SvgRectangleTranslator.GetRightLine(this.SvgRectangle);
+        this.LowerLine = this.SvgRectangleTranslator.GetLowerLine(this.SvgRectangle);
+        this.LeftLine = this.SvgRectangleTranslator.GetLeftLine(this.SvgRectangle);
+
+        this.ActualUpperLine = this.SvgLineTranslator.Translate(this.UpperLine);
+        this.ActualRightLine = this.SvgLineTranslator.Translate(this.RightLine);
+        this.ActualLowerLine = this.SvgLineTranslator.Translate(this.LowerLine);
+        this.ActualLeftLine = this.SvgLineTranslator.Translate(this.LeftLine);
+      }
+
+      [TestMethod]
+      public void translation_should_be_correct()
+      {
+        Assert.AreEqual("LO10,10,100,20",
+                        this.ActualUpperLine);
+        Assert.AreEqual("LO110,10,20,100",
+                        this.ActualRightLine);
+        Assert.AreEqual("LO10,110,100,20",
+                        this.ActualLowerLine);
+        Assert.AreEqual("LO10,10,20,100",
+                        this.ActualLeftLine);
+      }
+    }
+  }
+}
---
+++ src/System.Svg.Render.EPL/SvgUnitCalculator.cs
@@ -0,0 +1,48 @@
+﻿namespace System.Svg.Render.EPL
+{
+  public class SvgUnitCalculator
+  {
+    public SvgUnit Add(SvgUnit svgUnit1,
+                       SvgUnit svgUnit2)
+    {
+      this.CheckSvgUnitType(svgUnit1,
+                            svgUnit2);
+
+      var result = new SvgUnit(svgUnit1.Type,
+                               svgUnit1.Value + svgUnit2.Value);
+
+      return result;
+    }
+
+    public SvgUnit Substract(SvgUnit svgUnit1,
+                             SvgUnit svgUnit2)
+    {
+      this.CheckSvgUnitType(svgUnit1,
+                            svgUnit2);
+
+      var result = new SvgUnit(svgUnit1.Type,
+                               svgUnit1.Value - svgUnit2.Value);
+
+      return result;
+    }
+
+    private void CheckSvgUnitType(SvgUnit svgUnit1,
+                                  SvgUnit svgUnit2)
+    {
+      if (svgUnit1.Type != svgUnit2.Type)
+      {
+        // TODO add documentation
+        throw new ArgumentException($"{nameof(svgUnit2)}'s {nameof(SvgUnit.Type)} ({svgUnit2.Type}) does not equal {nameof(svgUnit1)}'s {nameof(SvgUnit.Type)} ({svgUnit1.Type})");
+      }
+    }
+
+    public int GetValue(SvgUnit svgUnit)
+    {
+      // TODO implement device-specific getting of .Value
+
+      var result = (int) svgUnit.Value;
+
+      return result;
+    }
+  }
+}
