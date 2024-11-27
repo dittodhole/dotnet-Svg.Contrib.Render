c2dfd66a981dbf98aa642ab49054c364e2978646
Andreas Niedermair <me+code@a76a6a65.nexus>
2024-11-27 23:50:49 +0100

    added tests for ZPL

---
+++ src/Svg.Contrib.Render.ZPL.Tests/SvgElementBaseTranslatorContext.cs
@@ -0,0 +1,47 @@
+﻿using System.Drawing.Drawing2D;
+using JetBrains.Annotations;
+using UnitTest;
+
+// ReSharper disable AutoPropertyCanBeMadeGetOnly.Global
+// ReSharper disable MemberCanBePrivate.Global
+
+namespace Svg.Contrib.Render.ZPL.Tests
+{
+  public abstract class SvgElementBaseTranslatorContext : ContextSpecification
+  {
+    protected SvgElementBaseTranslatorContext()
+    {
+      this.ZplContainer = new ZplContainer(new ZplStream(),
+                                           new ZplStream(),
+                                           new ZplStream());
+      this.SvgUnitReader = new SvgUnitReader();
+      this.ZplCommands = new ZplCommands();
+      this.ZplTransformer = new ZplTransformer(this.SvgUnitReader);
+      this.Matrix = new Matrix();
+    }
+
+    [NotNull]
+    protected ZplContainer ZplContainer { get; }
+
+    [NotNull]
+    protected SvgUnitReader SvgUnitReader { get; }
+
+    [NotNull]
+    protected ZplCommands ZplCommands { get; }
+
+    [NotNull]
+    protected ZplTransformer ZplTransformer { get; set; }
+
+    [NotNull]
+    protected Matrix Matrix { get; }
+
+    protected object Actual { get; private set; }
+
+    protected override void BecauseOf()
+    {
+      base.BecauseOf();
+
+      this.Actual = this.ZplContainer.ToString();
+    }
+  }
+}
