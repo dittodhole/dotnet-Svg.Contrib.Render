29fb53c2d5ed4d766fbd7e2ba8da0c7ac79bbc3b
Andreas Niedermair <me+code@a76a6a65.nexus>
2024-11-27 23:50:48 +0100

    removed unused code

--- src/System.Svg.Render.ZPL/ZplRenderer.cs
+++ src/System.Svg.Render.ZPL/ZplRenderer.cs
@@ -52,91 +52,10 @@ namespace System.Svg.Render.ZPL
       return encoding;
     }
 
-    //[NotNull]
-    //[Pure]
-    //[MustUseReturnValue]
-    //public virtual IEnumerable<ZplStream> GetInternalMemoryTranslation([NotNull] SvgDocument svgDocument)
-    //{
-    //  var parentMatrix = this.CreateParentMatrix();
-    //  var translations = this.TranslaveSvgElementAndChildrenForStoring(svgDocument,
-    //                                                                   parentMatrix,
-    //                                                                   this.ViewMatrix);
-
-    //  return translations;
-    //}
-
-    //[NotNull]
-    //[Pure]
-    //[MustUseReturnValue]
-    //protected virtual IEnumerable<ZplStream> TranslaveSvgElementAndChildrenForStoring([NotNull] SvgElement svgElement,
-    //                                                                                  [NotNull] Matrix parentMatrix,
-    //                                                                                  [NotNull] Matrix viewMatrix)
-    //{
-    //  var matrix = this.MultiplyTransformationsIntoNewMatrix(svgElement,
-    //                                                         parentMatrix);
-
-    //  {
-    //    var zplStream = this.TranslateSvgElementForStoring(svgElement,
-    //                                                       matrix,
-    //                                                       viewMatrix);
-    //    if (zplStream.Any())
-    //    {
-    //      yield return zplStream;
-    //    }
-    //  }
-
-    //  foreach (var child in svgElement.Children)
-    //  {
-    //    var translations = this.TranslaveSvgElementAndChildrenForStoring(child,
-    //                                                                     matrix,
-    //                                                                     viewMatrix);
-
-    //    foreach (var zplStream in translations)
-    //    {
-    //      if (zplStream == null)
-    //      {
-    //        continue;
-    //      }
-    //      if (zplStream.Any())
-    //      {
-    //        yield return zplStream;
-    //      }
-    //    }
-    //  }
-    //}
-
     [NotNull]
     [Pure]
     [MustUseReturnValue]
     protected virtual Matrix CreateParentMatrix() => new Matrix();
-
-    //[NotNull]
-    //[Pure]
-    //[MustUseReturnValue]
-    //protected virtual ZplStream TranslateSvgElementForStoring([NotNull] SvgElement svgElement,
-    //                                                          [NotNull] Matrix matrix,
-    //                                                          [NotNull] Matrix viewMatrix)
-    //{
-    //  var container = this.CreateZplStream();
-
-    //  var type = svgElement.GetType();
-
-    //  var svgElementToInternalMemoryTranslator = this.GetTranslator(type) as ISvgElementToInternalMemoryTranslator;
-    //  if (svgElementToInternalMemoryTranslator == null)
-    //  {
-    //    return container;
-    //  }
-
-    //  matrix = matrix.Clone();
-    //  matrix.Multiply(viewMatrix,
-    //                  MatrixOrder.Append);
-
-    //  svgElementToInternalMemoryTranslator.TranslateForStoring(svgElement,
-    //                                                           matrix,
-    //                                                           container);
-
-    //  return container;
-    //}
 
     [NotNull]
     [Pure]
