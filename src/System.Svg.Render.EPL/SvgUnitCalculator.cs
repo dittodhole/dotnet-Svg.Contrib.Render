9428b55692bb21796cd9466fd21cf7d5fbf25568
Andreas Niedermair <me+code@a76a6a65.nexus>
2024-11-27 23:50:38 +0100

    added sourceDpi to SvgUnitCalculator

--- src/System.Svg.Render.EPL/SvgUnitCalculator.cs
+++ src/System.Svg.Render.EPL/SvgUnitCalculator.cs
@@ -4,6 +4,16 @@
   {
     // TODO add reading for different origin
 
+    public SvgUnitCalculator()
+      : this(72) {}
+
+    public SvgUnitCalculator(int sourceDpi)
+    {
+      this.SourceDpi = sourceDpi;
+    }
+
+    public int SourceDpi { get; set; }
+
     public SvgUnit Add(SvgUnit svgUnit1,
                        SvgUnit svgUnit2)
     {
