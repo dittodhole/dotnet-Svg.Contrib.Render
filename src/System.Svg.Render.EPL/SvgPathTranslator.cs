0c50b935def0b8eb69b170eea49e965983dfe592
Andreas Niedermair <me+code@a76a6a65.nexus>
2024-11-27 23:54:00 +0100

    feat: extracted commands for SvgPath

--- src/System.Svg.Render.EPL/SvgPathTranslator.cs
+++ src/System.Svg.Render.EPL/SvgPathTranslator.cs
@@ -2,22 +2,24 @@
 using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Svg.Pathing;
-using System.Text;
 using JetBrains.Annotations;
 
 namespace System.Svg.Render.EPL
 {
-  public class SvgPathTranslator : SvgElementTranslatorWithEncoding<SvgPath>
+  public class SvgPathTranslator : SvgElementTranslatorBase<SvgPath>
   {
-    public SvgPathTranslator([NotNull] SvgLineTranslator svgLineTranslator,
-                             [NotNull] Encoding encoding)
-      : base(encoding)
+    public SvgPathTranslator([NotNull] Transformer transformer,
+                             [NotNull] EplCommands eplCommands)
     {
-      this.SvgLineTranslator = svgLineTranslator;
+      this.Transformer = transformer;
+      this.EplCommands = eplCommands;
     }
 
     [NotNull]
-    private SvgLineTranslator SvgLineTranslator { get; }
+    private Transformer Transformer { get; }
+
+    [NotNull]
+    private EplCommands EplCommands { get; }
 
     public override IEnumerable<byte> Translate([NotNull] SvgPath instance,
                                                 [NotNull] Matrix matrix)
@@ -31,18 +33,51 @@ namespace System.Svg.Render.EPL
       // TODO add test cases
 
       var result = instance.PathData.OfType<SvgLineSegment>()
-                           .Select(svgLineSegment => new SvgLine
-                                                     {
-                                                       Color = instance.Color,
-                                                       Stroke = instance.Stroke,
-                                                       StrokeWidth = instance.StrokeWidth,
-                                                       StartX = new SvgUnit(svgLineSegment.Start.X),
-                                                       StartY = new SvgUnit(svgLineSegment.Start.Y),
-                                                       EndX = new SvgUnit(svgLineSegment.End.X),
-                                                       EndY = new SvgUnit(svgLineSegment.End.Y)
-                                                     })
-                           .SelectMany(svgLine => this.SvgLineTranslator.Translate(svgLine,
-                                                                                   matrix));
+                           .SelectMany(svgLineSegment => this.TranslateSvgLineSegment(instance,
+                                                                                      svgLineSegment,
+                                                                                      matrix));
+
+      return result;
+    }
+
+    private IEnumerable<byte> TranslateSvgLineSegment([NotNull] SvgPath instance,
+                                                      [NotNull] SvgLineSegment svgLineSegment,
+                                                      [NotNull] Matrix matrix)
+    {
+      var svgLine = new SvgLine
+                    {
+                      Color = instance.Color,
+                      Stroke = instance.Stroke,
+                      StrokeWidth = instance.StrokeWidth,
+                      StartX = new SvgUnit(svgLineSegment.Start.X),
+                      StartY = new SvgUnit(svgLineSegment.Start.Y),
+                      EndX = new SvgUnit(svgLineSegment.End.X),
+                      EndY = new SvgUnit(svgLineSegment.End.Y)
+                    };
+
+      float startX;
+      float startY;
+      float endX;
+      float endY;
+      float strokeWidth;
+
+      this.Transformer.Transform(svgLine,
+                                 matrix,
+                                 out startX,
+                                 out startY,
+                                 out endX,
+                                 out endY,
+                                 out strokeWidth);
+
+      var horizontalStart = (int) startX;
+      var verticalStart = (int) startY;
+      var horizontalLength = (int) (endX - startX);
+      var verticalLength = (int) (endY - startY);
+
+      var result = this.EplCommands.LineDrawBlack(horizontalStart,
+                                                  verticalStart,
+                                                  horizontalLength,
+                                                  verticalLength);
 
       return result;
     }
---
+++ src/System.Svg.Render/Transformer.cs
@@ -0,0 +1,47 @@
+﻿using System.Drawing.Drawing2D;
+using JetBrains.Annotations;
+
+namespace System.Svg.Render
+{
+  public class Transformer
+  {
+    public Transformer([NotNull] ISvgUnitCalculator svgUnitCalculator)
+    {
+      this.SvgUnitCalculator = svgUnitCalculator;
+    }
+
+    [NotNull]
+    private ISvgUnitCalculator SvgUnitCalculator { get; }
+
+    public void Transform([NotNull] SvgLine svgLine,
+                          [NotNull] Matrix matrix,
+                          out float startX,
+                          out float startY,
+                          out float endX,
+                          out float endY,
+                          out float strokeWidth)
+    {
+      startX = this.SvgUnitCalculator.GetValue(svgLine.StartX);
+      startY = this.SvgUnitCalculator.GetValue(svgLine.StartY);
+      endX = this.SvgUnitCalculator.GetValue(svgLine.EndX);
+      endY = this.SvgUnitCalculator.GetValue(svgLine.EndY);
+      strokeWidth = this.SvgUnitCalculator.GetValue(svgLine.StrokeWidth);
+
+      this.SvgUnitCalculator.ApplyMatrix(startX,
+                                         startY,
+                                         matrix,
+                                         out startX,
+                                         out startY);
+
+      this.SvgUnitCalculator.ApplyMatrix(endX,
+                                         endY,
+                                         matrix,
+                                         out endX,
+                                         out endY);
+
+      this.SvgUnitCalculator.ApplyMatrix(strokeWidth,
+                                         matrix,
+                                         out strokeWidth);
+    }
+  }
+}
