02d5cbc3ecd4ab6af1c2f016760fef4f66cee221
Andreas Niedermair <me+code@a76a6a65.nexus>
2024-11-27 23:54:07 +0100

    added tests for ZPL

---
+++ src/Svg.Contrib.Render.ZPL.Tests/EplTransformer.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Annotations;
+
+namespace Svg.Contrib.Render.ZPL.Tests
+{
+  public class ZplTransformer : ZPL.ZplTransformer
+  {
+    public ZplTransformer([NotNull] SvgUnitReader svgUnitReader)
+      : base(svgUnitReader,
+             ZPL.ZplTransformer.DefaultOutputWidth,
+             ZPL.ZplTransformer.DefaultOutputHeight) {}
+
+    protected override float GetLineHeightFactor([NotNull] SvgTextBase svgTextBase) => 1f;
+  }
+}
---
+++ src/Svg.Contrib.Render.ZPL.Tests/SvgUnitReader.cs
@@ -0,0 +1,8 @@
+﻿namespace Svg.Contrib.Render.ZPL.Tests
+{
+  public class SvgUnitReader : Render.SvgUnitReader
+  {
+    public SvgUnitReader()
+      : base(0f) {}
+  }
+}
