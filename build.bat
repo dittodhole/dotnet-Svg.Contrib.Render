f5bb4fd75c299f7517f46fa4173fb60b9a380ed9
Andreas Niedermair <me+code@a76a6a65.nexus>
2024-11-27 23:54:02 +0100

    added nupkg restore

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
+powershell-nuget-packager\NuGet.exe restore src/System.Svg.Render.EPL.sln
 "%PROGRAMFILES(x86)%\MSBuild\14.0\Bin\MsBuild.exe" src/System.Svg.Render.EPL.sln
 powershell powershell-nuget-packager\Package.ps1
