1492bd94a3489304ce603b9c90f31e178348f013
Andreas Niedermair <me+code@a76a6a65.nexus>
2024-11-27 23:50:39 +0100

    added first raw SvgDocumentTranslator

---
+++ src/System.Svg.Render.EPL/SvgDocumentTranslator.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using Anotar.LibLog;
+
+namespace System.Svg.Render.EPL
+{
+  public class SvgDocumentTranslator : SvgElementTranslator<SvgDocument>
+  {
+    // TODO maybe switch to HybridDictionary - in this scenario we have just a bunch of translators, ... but ... community?!
+    private ConcurrentDictionary<Type, SvgElementTranslator> SvgElementTranslators { get; } = new ConcurrentDictionary<Type, SvgElementTranslator>();
+
+    public override object Translate(SvgDocument instance,
+                                     int targetDpi)
+    {
+      var translations = new List<object>();
+
+      this.TranslateSvgElementAndChildren(instance,
+                                          targetDpi,
+                                          translations);
+
+      var translation = string.Join(Environment.NewLine,
+                                    translations);
+
+      return translation;
+    }
+
+    private void TranslateSvgElementAndChildren(SvgElement svgElement,
+                                                int targetDpi,
+                                                List<object> translations)
+    {
+      this.TranslateSvgElement(svgElement,
+                               targetDpi,
+                               translations);
+
+      foreach (var child in svgElement.Children)
+      {
+        this.TranslateSvgElementAndChildren(child,
+                                            targetDpi,
+                                            translations);
+      }
+    }
+
+    protected virtual void TranslateSvgElement(SvgElement svgElement,
+                                               int targetDpi,
+                                               List<object> translations)
+    {
+      var type = svgElement.GetType();
+      var svgElementTranslator = this.GetSvgElementTranslator(type);
+      if (svgElementTranslator == null)
+      {
+        return;
+      }
+
+      var translation = svgElementTranslator.TranslateUntyped(svgElement,
+                                                              targetDpi);
+      translations.Add(translation);
+    }
+
+    public SvgElementTranslator GetSvgElementTranslator<T>() where T : SvgElement
+    {
+      return this.GetSvgElementTranslator(typeof(T));
+    }
+
+    public SvgElementTranslator GetSvgElementTranslator(Type type)
+    {
+      if (type == null)
+      {
+        LogTo.Error($"{nameof(type)} is null");
+        return null;
+      }
+
+      SvgElementTranslator svgElementTranslator;
+      this.SvgElementTranslators.TryGetValue(type,
+                                             out svgElementTranslator);
+      return svgElementTranslator;
+    }
+
+    public void AddSvgElementTranslator<T>(SvgElementTranslator<T> svgElementTranslator) where T : SvgElement
+    {
+      this.SvgElementTranslators[typeof(T)] = svgElementTranslator;
+    }
+
+    public void RemoveSvgElementTranslator<T>() where T : SvgElement
+    {
+      SvgElementTranslator svgElementTranslator;
+      this.SvgElementTranslators.TryRemove(typeof(T),
+                                           out svgElementTranslator);
+    }
+  }
+}
--- src/System.Svg.Render.EPL/SvgElementTranslator.cs
+++ src/System.Svg.Render.EPL/SvgElementTranslator.cs
@@ -1,8 +1,38 @@
-﻿namespace System.Svg.Render.EPL
+﻿using Anotar.LibLog;
+
+namespace System.Svg.Render.EPL
 {
-  public abstract class SvgElementTranslator<T>
+  public abstract class SvgElementTranslator
+  {
+    internal abstract object TranslateUntyped(object untypedInstance,
+                                              int targetDpi);
+  }
+
+  public abstract class SvgElementTranslator<T> : SvgElementTranslator
     where T : SvgElement
   {
+    internal override object TranslateUntyped(object untypedInstance,
+                                              int targetDpi)
+    {
+      if (untypedInstance == null)
+      {
+        LogTo.Error($"{nameof(untypedInstance)} is null");
+        return null;
+      }
+
+      var instance = untypedInstance as T;
+      if (instance == null)
+      {
+        LogTo.Error($"tried to translate {untypedInstance.GetType()} with {this.GetType()}");
+        return null;
+      }
+
+      var translation = this.Translate(instance,
+                                       targetDpi);
+
+      return translation;
+    }
+
     public abstract object Translate(T instance,
                                      int targetDpi);
   }
