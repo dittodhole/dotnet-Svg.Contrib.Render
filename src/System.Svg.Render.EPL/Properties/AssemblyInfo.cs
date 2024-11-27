17467270ad65b4c52545732c805b834df327fa65
Andreas Niedermair <me+code@a76a6a65.nexus>
2024-11-27 23:50:44 +0100

    adapted AssemblyInfo

--- src/System.Svg.Render.EPL/Properties/AssemblyInfo.cs
+++ src/System.Svg.Render.EPL/Properties/AssemblyInfo.cs
@@ -6,10 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("System.Svg.Render.EPL")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Convert SVG to Zebra EPL")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("System.Svg.Render.EPL")]
 [assembly: AssemblyCulture("")]
+[assembly: AssemblyCompany("Andreas Niedermair")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -29,5 +30,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]
