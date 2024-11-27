bc37d7c455f60ccd70a98aa2533544d3db245c23
Andreas Niedermair <me+code@a76a6a65.nexus>
2024-11-27 23:54:09 +0100

    added first commands for FingerPrint

---
+++ src/Svg.Contrib.Render.FingerPrint/Enums.cs
@@ -0,0 +1,13 @@
+﻿using JetBrains.Annotations;
+
+namespace Svg.Contrib.Render.FingerPrint
+{
+  [PublicAPI]
+  public enum PrintDirection
+  {
+    Normal = 4,
+    RotatedBy90Degrees = 3,
+    RotatedBy180Degrees = 2,
+    RotatedBy270Degrees = 1
+  }
+}
---
+++ src/Svg.Contrib.Render.FingerPrint/FingerPrintCommands.cs
@@ -0,0 +1,49 @@
+﻿using JetBrains.Annotations;
+
+// ReSharper disable ClassWithVirtualMembersNeverInherited.Global
+
+namespace Svg.Contrib.Render.FingerPrint
+{
+  [PublicAPI]
+  public class FingerPrintCommands
+  {
+    [NotNull]
+    [Pure]
+    [MustUseReturnValue]
+    public virtual string Position(int x,
+                                   int y)
+    {
+      // PRPOS
+      return $"PP {x},{y}";
+    }
+
+    [NotNull]
+    [Pure]
+    [MustUseReturnValue]
+    public virtual string Box(int height,
+                              int width,
+                              int lineWeight)
+    {
+      // PRBOX
+      return $"PX {height},{width},{lineWeight}";
+    }
+
+    [NotNull]
+    [Pure]
+    [MustUseReturnValue]
+    public virtual string Line(int length,
+                               int lineWeight)
+    {
+      // PRLINE
+      return $"PL {length},{lineWeight}";
+    }
+
+    [NotNull]
+    [Pure]
+    [MustUseReturnValue]
+    public virtual string Direction(PrintDirection printDirection)
+    {
+      return $"DIR {(int) printDirection}";
+    }
+  }
+}
