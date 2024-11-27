e4e34f8266c1dbf6c4c2b760e982cd4aec3286b6
Andreas Niedermair <me+code@a76a6a65.nexus>
2024-11-27 23:50:51 +0100

    added characterSet selection to FingerPrint

--- src/Svg.Contrib.Render.FingerPrint/Enums.cs
+++ src/Svg.Contrib.Render.FingerPrint/Enums.cs
@@ -28,4 +28,31 @@ namespace Svg.Contrib.Render.FingerPrint
     BottomMiddle = 2,
     BottomRight = 3
   }
+
+  [PublicAPI]
+  public enum CharacterSet
+  {
+    // TODO implement stuff
+    //Roman8 = 1,
+    Utf8 = 8,
+    //French = 33,
+    //Spanish = 34,
+    //English = 44,
+    //Swedish = 46,
+    //Norwegian = 47,
+    //German = 49,
+    //JapaneseLatin = 81,
+    //Portuguese = 351,
+    //PCMAP = -1,
+    //Latin1 = 850,
+    //Greek1 = 851,
+    //Latin2 = 852,
+    //Cyrillic = 855,
+    //Turkish = 857,
+    //Windows1250 = 1250,
+    //Windows1251 = 1251,
+    //Windows1252 = 1252,
+    //Windows1254 = 1254,
+    //Windows1257 = 1257
+  }
 }
