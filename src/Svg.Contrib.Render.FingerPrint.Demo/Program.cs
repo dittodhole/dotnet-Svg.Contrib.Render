00a3e7921f2ae68141ed8963b8c3d5307bca3e4e
Andreas Niedermair <me+code@a76a6a65.nexus>
2024-11-27 23:54:09 +0100

    init demo for FingerPrint

---
+++ src/Svg.Contrib.Render.FingerPrint.Demo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+
+// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable UnusedParameter.Local
+// ReSharper disable NonLocalizedString
+
+namespace Svg.Contrib.Render.FingerPrint.Demo
+{
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+      var file = "assets/label.svg";
+      var svgDocument = SvgDocument.Open(file);
+      var bootstrapper = new DefaultBootstrapper();
+      var fingerPrintRenderer = bootstrapper.BuildUp(90f,
+                                                     203f);
+
+      var stopwatch = Stopwatch.StartNew();
+      var fingerPrintContainer = fingerPrintRenderer.GetTranslation(svgDocument);
+      stopwatch.Stop();
+      Console.WriteLine(stopwatch.Elapsed);
+
+      var encoding = Encoding.Default;
+      var array = fingerPrintContainer.ToByteStream(encoding)
+                                      .ToArray();
+
+      using (var serialPort = new SerialPort("COM1",
+                                             115200,
+                                             Parity.None)
+                              {
+                                DataBits = 8,
+                                StopBits = StopBits.Two,
+                                Encoding = encoding
+                              })
+      {
+        serialPort.Open();
+        serialPort.Write(array,
+                         0,
+                         array.Count());
+      }
+    }
+  }
+}
