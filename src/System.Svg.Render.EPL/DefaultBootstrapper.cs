aa9df56407bf8fc5942ae6824559a4135aaf5de5
Andreas Niedermair <me+code@a76a6a65.nexus>
2024-11-27 23:53:56 +0100

    added DefaultBootstrapper

---
+++ src/System.Svg.Render.EPL/DefaultBootstrapper.cs
@@ -0,0 +1,27 @@
+﻿namespace System.Svg.Render.EPL
+{
+  public static class DefaultBootstrapper
+  {
+    public static SvgDocumentTranslator Create(int sourceDpi)
+    {
+      var svgUnitCalculator = new SvgUnitCalculator
+                              {
+                                SourceDpi = sourceDpi
+                              };
+
+      var svgLineTranslator = new SvgLineTranslator(svgUnitCalculator);
+
+      var svgRectangleTranslator = new SvgRectangleTranslator(svgLineTranslator,
+                                                              svgUnitCalculator);
+
+      var svgTextTranslator = new SvgTextTranslator(svgUnitCalculator);
+
+      var svgDocumentTranslator = new SvgDocumentTranslator();
+      svgDocumentTranslator.AddSvgElementTranslator(svgLineTranslator);
+      svgDocumentTranslator.AddSvgElementTranslator(svgRectangleTranslator);
+      svgDocumentTranslator.AddSvgElementTranslator(svgTextTranslator);
+
+      return svgDocumentTranslator;
+    }
+  }
+}
