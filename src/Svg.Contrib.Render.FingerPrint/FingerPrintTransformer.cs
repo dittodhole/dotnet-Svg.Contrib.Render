8184e57ed6344d6bfce30f58e583105a68bb870a
Andreas Niedermair <me+code@a76a6a65.nexus>
2024-11-27 23:50:50 +0100

    added FingerPrintTransformer

---
+++ src/Svg.Contrib.Render.FingerPrint/FingerPrintTransformer.cs
@@ -0,0 +1,23 @@
+﻿using JetBrains.Annotations;
+
+namespace Svg.Contrib.Render.FingerPrint
+{
+  [PublicAPI]
+  public class FingerPrintTransformer : GenericTransformer
+  {
+    public const int DefaultOutputHeight = 1296;
+    public const int DefaultOutputWidth = 816;
+
+    public FingerPrintTransformer([NotNull] SvgUnitReader svgUnitReader)
+      : base(svgUnitReader,
+             FingerPrintTransformer.DefaultOutputWidth,
+             FingerPrintTransformer.DefaultOutputHeight) {}
+
+    public FingerPrintTransformer([NotNull] SvgUnitReader svgUnitReader,
+                                  int outputWidth,
+                                  int outputHeight)
+      : base(svgUnitReader,
+             outputWidth,
+             outputHeight) {}
+  }
+}
