09d0bf7ae609633a874c5a52699eb3fadda0f52a
Andreas Niedermair <me+code@a76a6a65.nexus>
2024-11-27 23:50:47 +0100

    fixed build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-powershell-nuget-packager\NuGet.exe restore src/System.Svg.Render.EPL.sln
-"%PROGRAMFILES(x86)%\MSBuild\14.0\Bin\MsBuild.exe" src/System.Svg.Render.EPL.sln
+powershell-nuget-packager\NuGet.exe restore src/Svg.Contrib.Render.sln
+"%PROGRAMFILES(x86)%\MSBuild\14.0\Bin\MsBuild.exe" src/Svg.Contrib.Render.sln
 powershell powershell-nuget-packager\Package.ps1
